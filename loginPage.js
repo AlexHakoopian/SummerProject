49b28302661c168f4c6da4c7a98fd0dcf1908305
AlexHakoopian <alex_jh@hotmail.co.uk>
2018-06-05 22:10:03 +0100

    sorted stuff

--- loginPage.js
+++ loginPage.js
@@ -9,3 +9,13 @@ function login(username, password) {
 }else{
   alert("Password Wrong");
 }};
+
+// Get the modal
+var modal = document.getElementById('id01');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
