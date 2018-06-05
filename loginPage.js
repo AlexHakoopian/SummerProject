f79e4b5057dd3b601ef7ab9fd943aafb0c2484af
Jess <jessiah@live.co.uk>
2018-06-05 21:11:45 +0100

    qwdrr

--- loginPage.js
+++ loginPage.js
@@ -1,7 +1,7 @@
 
 function login(username, password) {
-  if(["jess". "alex", "Alex", "Jess"].includes(username)){
-    if(["thomas". "hakoopian"].includes(password)){
+  if(["jess", "alex", "Alex", "Jess"].includes(username)){
+    if(["thomas", "hakoopian"].includes(password)){
       window.location='testpage.html';
   }else{
     alert("Username Wrong");
